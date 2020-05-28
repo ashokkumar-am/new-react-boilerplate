958a578ba445f0341b93807509ae02d54abacfe0
AK <amashokkumar@hotmail.com>
2020-05-28 11:25:04 +0530

    AWS Cognito Auth Added and Tested

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Welcome to default React App Page</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
